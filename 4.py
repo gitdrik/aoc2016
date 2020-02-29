49d19eed6703cfdec900840494b961ed9e2bbb5c
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-02-29 17:33:08 +0100

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,22 @@
+from operator import itemgetter
+idsum = 0
+with open('4.txt') as f:
+    for line in f:
+        l = line.split('[')
+        chk = l[1][:-2]
+        n = int(l[0].split('-')[-1])
+        cs = sorted(''.join(l[0].split('-')[:-1]))
+        csn = []
+        while cs:
+            c = cs.pop(0)
+            cn = 1
+            while cs and cs[0] == c:
+                cn += 1
+                cs.pop(0)
+            csn.append((c,cn))
+        csn.sort(key=itemgetter(1),reverse=True)
+        nchk = ''.join([c[0] for c in csn[0:5]])
+        if nchk == chk:
+            idsum += n
+
+print(idsum)
