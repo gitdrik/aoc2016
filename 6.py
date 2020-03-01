a07aa2d72aa7a4fb21ed7398efb43dfe9299484e
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-03-01 11:29:56 +0100

    Add files via upload

--- 6.py
+++ 6.py
@@ -6,7 +6,7 @@ with open('6.txt') as f:
         for i, c in enumerate(line.strip()):
             msg[i] += c
 
-m1 = ''.join([collections.Counter(s).most_common(0)[0][0] for s in msg])
+m1 = ''.join([collections.Counter(s).most_common(1)[0][0] for s in msg])
 m2 = ''.join([collections.Counter(s).most_common()[-1][0] for s in msg])
 
 print('Part 1: ',m1)
